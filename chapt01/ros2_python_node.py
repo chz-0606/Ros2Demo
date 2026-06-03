2429566021e9f78423e174228208176693ed56d9
qq <2844095003@qq.com>
2026-06-03 21:28:15 +0800

    test

--- chapt01/ros2_python_node.py
+++ chapt01/ros2_python_node.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 def main():
   rclpy.init() #初始化工作，分配资源
   node = Node('python_node')
-  node.get_logger().info('节点已运行')  #export RCUTILS_CONSOLE_OUTPUT_FORMAT=[{function_name}:{line_number}]:{message}
+  node.get_logger().info('节点已运行!')  #export RCUTILS_CONSOLE_OUTPUT_FORMAT=[{function_name}:{line_number}]:{message}
   rclpy.spin(node)
   rclpy.shutdown()
 
