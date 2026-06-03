3ec56274a3346fae09ca08d644e739b78222a2f9
chz-0606 <2844095003@qq.com>
2026-06-03 14:35:33 +0800

    第一个节点试用

---
+++ chapt01/ros2_python_node.py
@@ -0,0 +1,12 @@
+import rclpy
+from rclpy.node import Node
+
+def main():
+  rclpy.init() #初始化工作，分配资源
+  node = Node('python_node')
+  node.get_logger().info('节点已运行')  #export RCUTILS_CONSOLE_OUTPUT_FORMAT=[{function_name}:{line_number}]:{message}
+  rclpy.spin(node)
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+  main()
